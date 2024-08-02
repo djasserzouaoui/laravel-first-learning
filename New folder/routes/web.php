c23559e90a45905e90da2743940ef761ece02185
djasserzouaoui <gndjasser@gmail.com>
2024-08-03 00:30:48 +0100

    web.php

--- New folder/routes/web.php
+++ New folder/routes/web.php
@@ -4,10 +4,18 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/','post.index')->name('home');
-Route::view('/login','Auth.login')->name('view.Login');
-Route::view('/register','Auth.register')->name('view.register');
-Route::view('/dashboard','Auth.dashboard')->name('view.dashboard');
 
-Route::post('/register',[AuthController::class,'register'])->name('register');
-Route::post('/login',[AuthController::class,'login'])->name('login');
-Route::post('/logout',[AuthController::class,'logout'])->name('logout');
+Route::middleware('auth')->group(function(){
+    Route::view('/dashboard','Auth.dashboard')->name('view.dashboard');
+});
+
+
+Route::middleware('guest')->group(function(){
+    Route::view('/login','Auth.login')->name('view.Login');
+    Route::view('/register','Auth.register')->name('view.register');
+
+    Route::post('/register',[AuthController::class,'register'])->name('register');
+    Route::post('/login',[AuthController::class,'login'])->name('login');
+    Route::post('/logout',[AuthController::class,'logout'])->name('logout');
+    
+});
